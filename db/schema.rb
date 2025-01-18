8e1b46b6db4a542e991b0a272de96842253e1df5
nathanielpierson <nathanielepierson@gmail.com>
2025-01-17 21:03:52 -0500

    add model, controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_16_013415) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_18_020145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "known_for"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
