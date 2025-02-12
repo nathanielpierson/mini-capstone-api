532aa83bb4c385a49ea6cf21186cfa75907aec83
nathanielpierson <nathanielepierson@gmail.com>
2025-02-11 20:51:28 -0500

    Add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
 
   post "/users" =>"users#create"
 
+  post "/sessions" => "sessions#create"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
