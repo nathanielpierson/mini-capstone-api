7997131fdcabd3d1a7454aaa5af256fc603ef1be
nathanielpierson <nathanielepierson@gmail.com>
2025-02-13 19:15:35 -0500

    add order model amd controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_12_011931) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_14_000933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_12_011931) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.integer "product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.decimal "subtotal", precision: 9, scale: 2
+    t.decimal "tax", precision: 9, scale: 2
+    t.decimal "total", precision: 9, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
