5c11aaec4fe94b3ecc4f78498b0c37ac28069876
nathanielpierson <nathanielepierson@gmail.com>
2025-02-18 20:28:14 -0500

    add category and categoryproduct models

---
+++ db/migrate/20250219012755_create_category_produccts.rb
@@ -0,0 +1,10 @@
+class CreateCategoryProduccts < ActiveRecord::Migration[7.2]
+  def change
+    create_table :category_produccts do |t|
+      t.integer :product_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
